d17a8032dc25167c3ea829b1b945bb7de761416a
fengsanan <18860374033@163.com>
2018-08-21 10:28:52 +0800

    初始化项目

---
+++ scripts/test_aaa.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+# 测试题目
+class TestAAA:
+    @pytest.mark.parametrize("a,b",[(3,6)])
+    def test_aaa(self,a):
+        print(a)
+
+
+
---
+++ scripts/test_login.py
@@ -0,0 +1,24 @@
+import pytest
+
+from page.login_page import LoginPage
+
+class TestLogin:
+
+    def setup(self):
+        self.login_page = LoginPage()
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    @pytest.mark.parametrize("username,password", [("zhangsan", "zhangsan123"), ("lisi", "lisi123")])
+    def test_login1(self, username, password):
+        self.login_page.input_username(username)
+        self.login_page.input_password(password)
+        self.login_page.click_login()
+
+        assert 0
+
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test_login2(self):
+        self.login_page.input_password("2")
+        self.login_page.input_username("2")
+        self.login_page.click_login()
+
+        assert 1
